4a71a989d1e590f51ebf5d52ce68582cd253d48a
biner107 <1070156025@qq.com>
2023-11-26 18:59:05 +0800

    add treesitter setting

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,25 +1,31 @@
 return {
-  -- "nvim-treesitter/nvim-treesitter",
-  -- opts = function(_, opts)
-  --   -- add more things to the ensure_installed table protecting against community packs modifying it
-  --   opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-  --     -- "lua"
-  --     "vue",
-  --   });
-  --   opts.highlight = {
-  --     enable = true,
-  --     additional_vim_regex_highlighting=true,
-  --     matchers = {
-  --       'tree_sitter#parser#isVueScript',
-  --     }
-  --   };
-  --   opts.incremental_selection = {
-  --     enable = true
-  --   };
-  --   opts.contextual_parser = {
-  --     enable = true
-  --   };
-  --   opts.indent = { enable = true };
-  --   opts.autotag = { enable = true };
-  -- end,
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    -- add more things to the ensure_installed table protecting against community packs modifying it
+    -- opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+    --   -- "lua"
+    --   "vue",
+    -- });
+    opts.highlight = {
+      enable = true,
+      additional_vim_regex_highlighting=true,
+      matchers = {
+        'tree_sitter#parser#isVueScript',
+      }
+    };
+    opts.incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<CR>", -- set to `false` to disable one of the mappings
+        node_incremental = "<CR>",
+        scope_incremental = "<TAB>",
+        node_decremental = "<BS>",
+      },
+    };
+    -- opts.contextual_parser = {
+    --   enable = true
+    -- };
+    -- opts.indent = { enable = true };
+    -- opts.autotag = { enable = true };
+  end,
 }
