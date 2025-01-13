125fec763cef08c077f6246e02cfae5dcb00ce1c
biner107 <1070156025@qq.com>
2025-01-13 16:46:01 +0800

    新增ai支持

--- polishs/init.lua
+++ polishs/init.lua
@@ -2,7 +2,31 @@ return function()
     require "user.polishs.filetypes".extendfiletype()
     require "user.polishs.command"
     require "user.polishs.buffer".setup()
-    -- require "user.polishs.findcode".setup()
-    -- vim.cmd([[ au User LspNotify setup ]])
-    -- Set up custom filetypes
+    local openai = require('user.polishs.openai');
+    openai.setup({})
+    vim.keymap.set('v', '<C-g>r', openai.replace, {
+      silent = true,
+      noremap = true,
+      desc = "[G]pt [R]ewrite"
+    })
+    vim.keymap.set('v', '<C-g>p', openai.visual_prompt, {
+      silent = false,
+      noremap = true,
+      desc = "[G]pt [P]rompt"
+    })
+    vim.keymap.set('n', '<C-g>p', openai.prompt, {
+      silent = true,
+      noremap = true,
+      desc = "[G]pt [P]rompt"
+    })
+    vim.keymap.set('n', '<C-g>c', openai.cancel, {
+      silent = true,
+      noremap = true,
+      desc = "[G]pt [C]ancel"
+    })
+    vim.keymap.set('i', '<C-g>p', openai.prompt, {
+      silent = true,
+      noremap = true,
+      desc = "[G]pt [P]rompt"
+    })
 end
